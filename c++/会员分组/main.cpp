313aae42841c1a17a165302fde9376892e9f770b
xun <131086678@qq.com>
2018-03-12 13:47:01 +0800

    Initial commit

---
+++ c++/会员分组/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+int W,count=0,i=1,temp;
+int a[10]={0};
+cout<<"Please input the W: "<<endl;
+cin>>W;
+
+temp=W;
+
+while(temp!=0) {temp/=10;count++;}
+
+while((W/10)!=0)
+{
+a[count-i]=W%10;
+W/=10;
+i++;
+}
+a[0]=W;
+
+for(i=0;i<count;i++)
+cout<<a[i];
+}

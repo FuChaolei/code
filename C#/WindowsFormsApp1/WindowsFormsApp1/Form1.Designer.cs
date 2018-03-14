975f5569ca57685ccbd7e58b6f4e468dde442872
xun <131086678@qq.com>
2018-03-14 22:39:44 +0800

    初始界面

--- C#/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ C#/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -41,60 +41,64 @@
             // 
             this.button1.Location = new System.Drawing.Point(200, 240);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 44);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "登录";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(440, 239);
+            this.button2.Location = new System.Drawing.Point(400, 240);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(75, 44);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "取消";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(272, 163);
+            this.textBox1.Location = new System.Drawing.Point(316, 163);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(159, 25);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(272, 127);
+            this.textBox2.Location = new System.Drawing.Point(316, 127);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.Size = new System.Drawing.Size(159, 25);
             this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(200, 127);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.Size = new System.Drawing.Size(37, 15);
             this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.label1.Text = "账号";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(200, 173);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.Size = new System.Drawing.Size(37, 15);
             this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label2.Text = "密码";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(203, 88);
+            this.label3.Location = new System.Drawing.Point(200, 72);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.Size = new System.Drawing.Size(67, 15);
             this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.label3.Text = "教务系统";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
             // 
@@ -110,6 +114,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- C#/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ C#/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,35 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username, password;
+            username = textBox2.Text;
+            password = textBox1.Text;
+            if (username == "付超磊" && password == "123")
+            {
+                MessageBox.Show("登陆成功！");
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+            else
+                MessageBox.Show("登录失败！");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }

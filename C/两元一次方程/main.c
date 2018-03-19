957057c7925b4fdf7e40e7032d5c0a9af514fba7
xun <131086678@qq.com>
2018-03-19 13:59:09 +0800

    3.19

--- C/两元一次方程/main.c
+++ C/两元一次方程/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
 int main()
 {
-    float a,b,c;
-    scanf("%f%f%f",&a,&b,&c);
+    float a,b,c,delt,x1,x2;
+    printf("输入方程的三个系数:");
+    scanf("%f %f %f",&a,&b,&c);
+    delt = b*b-4*a*c;
+    x1 = (-b+sqrt(delt))/(2*a);
+    x2 = (-b-sqrt(delt))/(2*a);
+    printf("方程的解分别为：x1=%.2f  x2=%.2f\n",x1,x2);
     return 0;
 }
